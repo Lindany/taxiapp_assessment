d6affe54090292fce419bac1865462328cbed0cd
Lindany <mabasolindaniricardo@gmail.com>
2020-05-30 04:40:46 +0200

    build file

---
+++ taxi-app/precache-manifest.579f3785669d7571ca7ab2d1bffa7d1e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6332c2aea40f00841498c59bb71650a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dabe36d5c6e4f212cc6",
+    "url": "/static/css/2.c4044783.chunk.css"
+  },
+  {
+    "revision": "ec835debc9248bdf3f2a",
+    "url": "/static/css/main.a3b64a26.chunk.css"
+  },
+  {
+    "revision": "e71aacefa93c25c19622",
+    "url": "/static/js/10.75cfb703.chunk.js"
+  },
+  {
+    "revision": "fd0d3854b36030251e84",
+    "url": "/static/js/11.e4ff91a1.chunk.js"
+  },
+  {
+    "revision": "9137618dabf59b670151",
+    "url": "/static/js/12.1fe0be9b.chunk.js"
+  },
+  {
+    "revision": "2317d0fcb0a599ca6c4b",
+    "url": "/static/js/13.2401dd5e.chunk.js"
+  },
+  {
+    "revision": "35fcf263c048854771d5",
+    "url": "/static/js/14.4f629deb.chunk.js"
+  },
+  {
+    "revision": "1dabe36d5c6e4f212cc6",
+    "url": "/static/js/2.add4515e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.add4515e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917cb8f832c3ca5683e0",
+    "url": "/static/js/3.5fbfe9e9.chunk.js"
+  },
+  {
+    "revision": "3a583bb2fa67ffa38cb1",
+    "url": "/static/js/4.5e155a67.chunk.js"
+  },
+  {
+    "revision": "63a9e3c4ca421db44162",
+    "url": "/static/js/5.481604d5.chunk.js"
+  },
+  {
+    "revision": "a7dec654328021e343b4",
+    "url": "/static/js/6.086c1d90.chunk.js"
+  },
+  {
+    "revision": "58dcc725450c912a5044",
+    "url": "/static/js/7.744a43ec.chunk.js"
+  },
+  {
+    "revision": "407b1ee57059ef59d654",
+    "url": "/static/js/8.287a9b0a.chunk.js"
+  },
+  {
+    "revision": "629fc759af669167b163",
+    "url": "/static/js/9.24b0f66a.chunk.js"
+  },
+  {
+    "revision": "ec835debc9248bdf3f2a",
+    "url": "/static/js/main.96c06bc3.chunk.js"
+  },
+  {
+    "revision": "33ce769a0a23a196423a",
+    "url": "/static/js/runtime-main.d82edf03.js"
+  },
+  {
+    "revision": "06b428b1e221d3b3d36d2edb6f67c14a",
+    "url": "/static/media/banner2.06b428b1.jpg"
+  },
+  {
+    "revision": "c17bb03fef106bd49c4628a3717c68c6",
+    "url": "/static/media/pretoriaTvbanner.c17bb03f.jpg"
+  }
+]);
