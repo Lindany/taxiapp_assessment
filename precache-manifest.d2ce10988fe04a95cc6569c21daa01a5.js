0edc59f2d9d9dc96f31509aa7460441f5e4f9896
Lindany <mabasolindaniricardo@gmail.com>
2020-06-11 16:52:38 +0200

    add message

---
+++ precache-manifest.d2ce10988fe04a95cc6569c21daa01a5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319f2e1ee7ee1e685a5c0ca59f448d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1572eb45f227e2456d8",
+    "url": "/static/css/2.717a14fe.chunk.css"
+  },
+  {
+    "revision": "c8cf28b8250b508c6263",
+    "url": "/static/css/main.e669448d.chunk.css"
+  },
+  {
+    "revision": "9c6f1ef8e04cdbf7ad65",
+    "url": "/static/js/10.cf154110.chunk.js"
+  },
+  {
+    "revision": "044d78a88cb6ac1e4a79",
+    "url": "/static/js/11.f972ac7a.chunk.js"
+  },
+  {
+    "revision": "5873ec2f0ddbd1f13ce0",
+    "url": "/static/js/12.765719f7.chunk.js"
+  },
+  {
+    "revision": "94ad709b23d88db8f514",
+    "url": "/static/js/13.ef48c985.chunk.js"
+  },
+  {
+    "revision": "0c5e74d90b13ddb73c0e",
+    "url": "/static/js/14.93c3dd5d.chunk.js"
+  },
+  {
+    "revision": "e1572eb45f227e2456d8",
+    "url": "/static/js/2.66360d4c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.66360d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a93a4ee02ec03a7e1c",
+    "url": "/static/js/3.673e643b.chunk.js"
+  },
+  {
+    "revision": "714818309ebcc9f658f9",
+    "url": "/static/js/4.3288f454.chunk.js"
+  },
+  {
+    "revision": "78f6ed4489fcb8883c20",
+    "url": "/static/js/5.f763d904.chunk.js"
+  },
+  {
+    "revision": "347dfd93859794d74df9",
+    "url": "/static/js/6.d30e0c8e.chunk.js"
+  },
+  {
+    "revision": "db1a47574bfd5d4bc8f3",
+    "url": "/static/js/7.3f421f3d.chunk.js"
+  },
+  {
+    "revision": "032ca99f7a62ce1a252e",
+    "url": "/static/js/8.970f6c42.chunk.js"
+  },
+  {
+    "revision": "a4f0db9515e7b6056a16",
+    "url": "/static/js/9.ee610aea.chunk.js"
+  },
+  {
+    "revision": "c8cf28b8250b508c6263",
+    "url": "/static/js/main.18d8d528.chunk.js"
+  },
+  {
+    "revision": "353d9abe6d609f491e74",
+    "url": "/static/js/runtime-main.b028b34c.js"
+  },
+  {
+    "revision": "dc3475cf16ac75f2149b1702fcba0781",
+    "url": "/static/media/CARTOON.dc3475cf.jpg"
+  },
+  {
+    "revision": "ce99e9e3a03edca54be649cc3e855a7d",
+    "url": "/static/media/CURRENT.ce99e9e3.jpg"
+  },
+  {
+    "revision": "950c680852f83b82d5b2653869e1d353",
+    "url": "/static/media/MOVIE.950c6808.jpg"
+  },
+  {
+    "revision": "4db0b4dd55ec8ecf954c25184246886a",
+    "url": "/static/media/MUSIC.4db0b4dd.jpg"
+  },
+  {
+    "revision": "bc2d421c5bae4c392a194f06259ddc50",
+    "url": "/static/media/SERIES.bc2d421c.jpg"
+  },
+  {
+    "revision": "d13c1659c9367df40df3e78a2793c3b0",
+    "url": "/static/media/SPORTS.d13c1659.jpg"
+  },
+  {
+    "revision": "06b428b1e221d3b3d36d2edb6f67c14a",
+    "url": "/static/media/banner2.06b428b1.jpg"
+  },
+  {
+    "revision": "c17bb03fef106bd49c4628a3717c68c6",
+    "url": "/static/media/pretoriaTvbanner.c17bb03f.jpg"
+  },
+  {
+    "revision": "bc2d421c5bae4c392a194f06259ddc50",
+    "url": "/static/media/tax tv series.bc2d421c.jpg"
+  }
+]);
